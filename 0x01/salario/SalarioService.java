b5ec8b5a0f2468a4422e8bc1a719390eaad7525b
Marcos Galvão <mgalvao@ciandt.com>
2022-07-20 13:06:53 -0300

    adjusts to pass checker

--- 0x01/salario/SalarioService.java
+++ 0x01/salario/SalarioService.java
@@ -2,18 +2,18 @@ import java.util.UUID;
 
 public class SalarioService {
     
-    private CalculadorSalarioService calculator = CalculadorSalarioService.getCalculadorSalarioService();
+    private CalculadorSalarioService calculador = CalculadorSalarioService.getCalculadorSalarioService();
 
     public double calcular(double salarioBruto, 
                         double valorDescontos, 
                         double valorVendas,
                         double percentualComissao) {
         
-        return calculator.calcularSalarioLiquido(salarioBruto, valorDescontos, valorVendas, percentualComissao);
+        return calculador.calcularSalarioLiquido(salarioBruto, valorDescontos, valorVendas, percentualComissao);
     }
 
     public UUID getUuid() {
-        return calculator.uuid;
+        return calculador.uuid;
     }
 
 }
