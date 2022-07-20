b01356431f672887d3e7931d9f10cd7f7e6d9a66
Marcos Galvão <mgalvao@ciandt.com>
2022-07-20 16:54:27 -0300

    task 1 Design Patterns part 2

---
+++ 0x01/cafeteria/BebidaCafe.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BebidaCafe extends Bebida {
+
+    @Override
+    public List<String> obterIngredientes() {
+        return new ArrayList<String>(Arrays.asList("cafe"));
+    }
+
+    @Override
+    public double obterPreco() {
+        return 5.35;
+    }
+
+}
